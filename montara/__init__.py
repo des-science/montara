2e7854909e2a20927e593a27b739490937abf643
Matthew R. Becker <becker.mr@gmail.com>
2021-04-14 14:36:46 -0500

    BUG remove some imports

--- montara/__init__.py
+++ montara/__init__.py
@@ -4,8 +4,6 @@ from . import input_desstar
 from . import catalogsampler
 from . import catchffterror_stamp
 from . import utils
-from . import des_psfex_perturbed
-from . import bdf_object
 from . import z_slice_shear
 from . import mixed_scene_postop
 from . import des_piff
