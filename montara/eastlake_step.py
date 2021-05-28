998426be475532a94fc4700d6ceeadc6da36c594
Masaya Yamamoto <my137@duke.edu>
2021-05-28 13:03:25 +0900

    is it in output?

--- montara/eastlake_step.py
+++ montara/eastlake_step.py
@@ -261,8 +261,8 @@ class MontaraGalSimRunner(Step):
                     # if doing gridded objects, save the true position data
                     # to a fits file
                     if config['output'].get('grid_objects', False):
-                        print(stash)
-                        _pos_data = config['grid_objects_pos_data']
+                        print(output)
+                        _pos_data = output['grid_objects_pos_data']
                         truepos_filename = os.path.join(
                             base_dir,
                             "true_positions",
