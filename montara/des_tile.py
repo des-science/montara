1f001be6f79356f570fc6dbabad4d14aa4ebb105
Masaya Yamamoto <my137@duke.edu>
2022-06-14 11:30:35 +0900

    WIP shear the full scene

--- montara/des_tile.py
+++ montara/des_tile.py
@@ -711,8 +711,8 @@ class DESTileBuilder(OutputBuilder):
                 pos = np.vstack((u, v))
                 sheared_uv = np.dot(S, pos)
                 print('tile center', tile_setup["tile_center"])
-                np.save('~/DES/des-y6-analysis/sheared_u.npy', sheared_uv[0,:])
-                np.save('~/DES/des-y6-analysis/sheared_v.npy', sheared_uv[1,:])
+                np.save('/global/homes/m/myamamot/DES/eastlake/eastlake/examples/sheared_u.npy', sheared_uv[0,:])
+                np.save('/global/homes/m/myamamot/DES/eastlake/eastlake/examples/sheared_v.npy', sheared_uv[1,:])
                 # convert sheared u,v back to sheared ra,dec
                 sheared_ra, sheared_dec = tile_setup["tile_center"].deproject_rad(sheared_uv[0,:], sheared_uv[1,:], projection='gnomonic')
                 print(len(sheared_ra))
