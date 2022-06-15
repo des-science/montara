121b6ed697953b900aa4eaf33577ade164c8d7c9
Masaya Yamamoto <my137@duke.edu>
2022-06-15 14:25:17 +0900

    WIP shear full scene, non-grid

--- montara/des_tile.py
+++ montara/des_tile.py
@@ -840,6 +840,7 @@ class DESTileBuilder(OutputBuilder):
         # why we are not using super(...).
         images = OutputBuilder.buildImages(
             self, config, base, file_num, image_num, obj_num, ignore, logger)
+        print('images', images)
         return images
 
     def writeFile(self, data, file_name, config, base, logger):
