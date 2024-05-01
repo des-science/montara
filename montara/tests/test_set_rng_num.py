ee01ce06fbda784034790ede5ebc8f01f4cb7ea9
Matthew R Becker <becker.mr@gmail.com>
2024-05-01 12:30:47 -0400

    STY please the flake8

--- montara/tests/test_set_rng_num.py
+++ montara/tests/test_set_rng_num.py
@@ -3,7 +3,11 @@ from montara.des_tile import _set_catalog_sampler_rng_num
 
 
 def test_set_catalog_sampler_rng_num():
-    cfg = {"blah": {"foo": [{"type": "catalog_sampler_value"}]}, "blahblah": {"type": "blah"}, "bar": {"type": "catalog_sampler_value"}}
+    cfg = {
+        "blah": {"foo": [{"type": "catalog_sampler_value"}]},
+        "blahblah": {"type": "blah"},
+        "bar": {"type": "catalog_sampler_value"},
+    }
     new_cfg = _set_catalog_sampler_rng_num(copy.deepcopy(cfg), 1)
     assert new_cfg == {
         "blah": {
@@ -18,7 +22,11 @@ def test_set_catalog_sampler_rng_num():
         }
     }
 
-    cfg = {"blah": {"foo": [{"type": "catalog_sampler_value"}]}, "blahblah": {"type": "blah"}, "bar": {"type": "catalog_sampler_value"}}
+    cfg = {
+        "blah": {"foo": [{"type": "catalog_sampler_value"}]},
+        "blahblah": {"type": "blah"},
+        "bar": {"type": "catalog_sampler_value"},
+    }
     cfg = _set_catalog_sampler_rng_num(cfg, 1)
     assert cfg == {
         "blah": {
@@ -32,4 +40,3 @@ def test_set_catalog_sampler_rng_num():
             "rng_num": 1
         }
     }
-
